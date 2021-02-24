889a3d4dc5e988b519be0559e6883f538ee4176a
Алена Коломиец <alenakolomiec@MacBook-Air-Alena.local>
2021-02-24 14:08:42 +0200

    added proptype children

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
+  console.log(positivePercentage);
   return (
     <>
       <ul>
